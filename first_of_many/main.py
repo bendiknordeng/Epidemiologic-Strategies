4a966ede23e27a2b17d3c78572365b4ae514cd1b
sandernordeide <nordeidesander@gmail.com>
2021-03-05 15:33:03 +0100

    Add several regions to main

--- first_of_many/main.py
+++ first_of_many/main.py
@@ -1,7 +1,39 @@
 from region import Region
 
-if __name__ == '__main__':
-    N = 681067
-    oslo = Region(0, 681067, 681000, 67, 0, 59.91273, 10.74609)
-    oslo.model_outbreak()
+regions = {}
+region_id = 0
+N = 681067
+initial_infected = 0.05
+longitude = 59.91273
+latitude = 10.74609
+regions[0] = Region(region_id, N, initial_infected, longitude, latitude)
+
+region_id = 1
+N = 41460
+initial_infected = 0
+longitude = 59.924507
+latitude = 10.954048
+regions[1] = Region(region_id, N, initial_infected, longitude, latitude)
+
+region_id = 2
+N = 127731
+initial_infected = 0
+longitude = 59.89455
+latitude = 10.546343
+regions[2] = Region(region_id, N, initial_infected, longitude, latitude)
+
+region_id = 3
+N = 59288
+initial_infected = 0
+longitude = 59.735206
+latitude = 10.908211
+regions[3] = Region(region_id, N, initial_infected, longitude, latitude)
+
+"""
+mobility_matrix = [ [0,0.1,0.1,0.1],
+                    [0.5,0,0,0],
+                    [0.5,0,0,0],
+                    [0.5,0,0,0]]
+"""
+
     
--- first_of_many/region.py
+++ first_of_many/region.py
@@ -1,15 +1,16 @@
 from SIR import SIR
+import numpy as np
 
 class Region:
-    def __init__(self, region_id, n_inhabitants, s0, i0, r0, longitude, latitude) -> None:
+    def __init__(self, region_id, n_inhabitants, initial_infected, longitude, latitude) -> None:
         # Demographics parameters
         self.region_id = region_id
         self.n_inhabitants = n_inhabitants
         
         # Epidemiological model parameters
-        self.s0 = s0
-        self.i0 = i0
-        self.r0 = r0
+        self.s0 = int(np.ceil(self.n_inhabitants*0.95))
+        self.i0 = int(np.floor(self.n_inhabitants*0.05))
+        self.r0 = 0
         
         # Geographical parameters
         self.longitude = longitude
@@ -17,6 +18,7 @@ class Region:
 
         # SIR-model 
         self.sir_model = SIR()
+    
     
     def model_outbreak(self):
         self.sir_model.outbreak(self.s0, self.i0, self.r0)
