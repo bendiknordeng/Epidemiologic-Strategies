2d669f3e0fc032397426dabd477b843579a7f22a
Peder Lund <peder.m.lund@gmail.com>
2021-03-05 22:07:13 +0100

    small update

--- first_of_many/region.py
+++ first_of_many/region.py
@@ -1,5 +1,4 @@
-#from SIR import SIR
-from spread import SIR
+from SIR import SIR
 import numpy as np
 
 class Region:
@@ -17,20 +16,11 @@ class Region:
         self.longitude = longitude
         self.latitude = latitude
 
-        # SIR-model
-        kwargs = {"beta":1, 
-            "p":0.05, 
-            "sigma":0.1, 
-            "time_steps":100, 
-            "dt":1,
-            "y0":[10000, 100, 0] # S0, I0, R0 
-            }
-     
-        self.sir_model = SIR(**kwargs)
+        # SIR-model 
+        self.sir_model = SIR()
     
     
     def model_outbreak(self):
-        self.sir_model
-        self.sir_model.simulate_epidemic()
-        self.sir_model.plot_simulation()
+        self.sir_model.outbreak(self.s0, self.i0, self.r0)
+        self.sir_model.plot_outbreak()
     
