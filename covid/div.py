61f7ca1645a0100f37090bde72e3a22eda331c8a
PederML <43098910+PederML@users.noreply.github.com>
2021-03-14 16:57:45 +0100

    Implemented vaccination in SEIR model
    Implemented vaccination in SEIR model and plots

---
+++ covid/div.py
@@ -0,0 +1,74 @@
+import pickle
+import numpy as np
+
+def generate_od_matrix(num_counties=11, num_time_steps=84):
+    """ generate an OD-matrix used for illustrative purposes only
+    Paramters:
+        num_counties:
+        num_time_steps:
+    Returns:
+        An OD-matrix with dimensions (num_time_steps, num_counties, num_counties) with 0.8 on its diagonal and 0.1 on cells next to diagonal 
+    """
+    a = []
+    for m in range(num_time_steps):
+        l = [[0 for x in range(num_counties)] for x in range(num_counties)] 
+        for i in range(0, num_counties):
+            for j in range(0, num_counties):
+                if i == j:
+                    l[i][j] = 0.8
+                elif j == i+1:
+                    l[i][j] = 0.1
+                elif j == i-1:
+                    l[i][j] = 0.1
+        a.append(l)
+    return np.array(a)
+
+def generate_vaccine_matrix(num_counties=11, num_time_steps=84):
+    """ generate an vaccine matrix used for illustrative purposes only
+    Paramters:
+        num_counties:
+        num_time_steps:
+    Returns:
+        An vaccine matrix with dimensions (num_time_steps, num_counties) 
+    """
+    a = [[100 for x in range(num_counties)] for i in range (num_time_steps)]
+    return np.array(a)
+    
+def write_pickle(filepath, arr):
+    """ writes arr as a pickle to filepath
+    Paramters:
+        filepath: string filepath
+        arr: array that is written to file 
+    """
+    with open(filepath,'wb') as f:
+        pickle.dump(arr, f)
+
+def read_pickle(filepath):
+    """ read pickle from filepath
+    Paramters:
+        filepath: string filepath
+    Returns:
+        arr: array that is read from filepath 
+    """
+    with open(filepath,'rb') as f:
+        return pickle.load(f)
+
+
+def main():
+    # f = 'covid/data/data_counties/od_counties.pkl'
+    # od = generate_od_matrix()
+    # write_pickle(f, od)
+    # x = read_pickle(f)
+    # print(x[0])
+    # print(x.shape)
+
+
+    v = 'covid/data/data_counties/vaccines_counties.pkl'
+    m = generate_vaccine_matrix()
+    write_pickle(v, m)
+    y = read_pickle(v)
+    # print(y)
+    # print(y.shape)
+
+if __name__ == '__main__':
+    main()
--- covid/virus-sim.py
+++ covid/virus-sim.py
@@ -7,23 +7,27 @@
 
 import numpy as np
 from collections import namedtuple
+import pickle
+import pandas as pd
+import matplotlib.pyplot as plt
 
-Param = namedtuple('Param', 'R0 DE DI I0 HospitalisationRate HospitalIters')
+Param = namedtuple('Param', 'R0 DE DI I0 HospitalisationRate HospitalIters eff')
 # I0 is the distribution of infected people at time t=0, if None then randomly choose inf number of people
 
 # flow is a 3D matrix of dimensions r x n x n (i.e., 84 x 549 x 549),
 # flow[t mod r] is the desired OD matrix at time t.
 
-def seir(par, distr, flow, alpha, iterations, inf):
+def seir(par, distr, flow, alpha, iterations, inf, vacc):
     """ Simulates an epidemic
     Parameters:
         - par: parameters {
                 R0: Basic reproduction number (e.g 2.4)
                 DE: Incubation period (e.g 5.6 * 12)        # Needs to multiply by 12 to get one day effects
                 DI: Infectious period (e.g 5.2 * 12)
-                I0: In(eitial infectiouns .g initial)
+                I0: Array with the distribution of infected people at time t=0
                 HospitalisationRate: Percentage of people that will be hospitalized (e.g 0.1)
                 HospitalIters: Length of hospitalization (e.g 15*12) }
+                eff: vaccine efficacy (e.g 0.95)
         - distr: population distribution
         - flow: OD matrices with dimensions r x n x n (i.e., 84 x 549 x 549).  flow[t mod r] is the desired OD matrix at time t. Use mod in order to only need one week of OD- matrices. 
         - alpha: strength of lock down measures/movement restriction. value of 1 - normal flow, 0 - no flow 
@@ -34,6 +38,7 @@ def seir(par, distr, flow, alpha, iterations, inf):
         - history: matrix with the number of subjects in each compartment [sim_it, compartment_id, num_cells]
     """
     
+    k = 6 # Num of compartments
     r = flow.shape[0]
     n = flow.shape[1]
     N = distr[0].sum() # total population, we assume that N = sum(flow)
@@ -42,6 +47,7 @@ def seir(par, distr, flow, alpha, iterations, inf):
     Evec = np.zeros(n)
     Ivec = np.zeros(n)
     Rvec = np.zeros(n)
+    Vvec = np.zeros(n)
     
     if par.I0 is None:
         initial = np.zeros(n)
@@ -50,7 +56,7 @@ def seir(par, distr, flow, alpha, iterations, inf):
             loc = np.random.randint(n)
             if (Svec[loc] > initial[loc]):
                 initial[loc] += 1.0
-                
+
     else:
         initial = par.I0
     assert ((Svec < initial).sum() == 0)
@@ -58,18 +64,20 @@ def seir(par, distr, flow, alpha, iterations, inf):
     Svec -= initial
     Ivec += initial
     
-    res = np.zeros((iterations, 5))
-    res[0,:] = [Svec.sum(), Evec.sum(), Ivec.sum(), Rvec.sum(), 0]
+    res = np.zeros((iterations, k))
+    res[0,:] = [Svec.sum(), Evec.sum(), Ivec.sum(), Rvec.sum(), 0, Vvec.sum()]
     
     realflow = flow.copy() # copy!
-    realflow = realflow / realflow.sum(axis=2)[:,:, np.newaxis]    
+    realflow = realflow / realflow.sum(axis=2)[:,:, np.newaxis]  # Normalize the flow
     realflow = alpha * realflow 
     
-    history = np.zeros((iterations, 5, n))
+    history = np.zeros((iterations, k, n))
     history[0,0,:] = Svec
     history[0,1,:] = Evec
     history[0,2,:] = Ivec
     history[0,3,:] = Rvec
+    history[0,5,:] = Vvec
+
     
     eachIter = np.zeros(iterations + 1)
     
@@ -77,20 +85,25 @@ def seir(par, distr, flow, alpha, iterations, inf):
     for iter in range(0, iterations - 1):
         realOD = realflow[iter % r]
         
-        d = distr[iter % r] + 1
+        v = vacc[iter % r] + 1
+        v = np.minimum(v, Svec)[0] # Ensure that no people are vaccinated if they are not suceptible
+    
+        d = distr[iter % r] + 1  # At least one person in each cell.
         
         if ((d>N+1).any()): #assertion!
-            print("Houston, we have a problem!")
+            print("Population does not stay constant!")
             return res, history
             
         newE = Svec * Ivec / d * (par.R0 / par.DI)
         newI = Evec / par.DE
         newR = Ivec / par.DI
+        newV = v * par.eff
         
         Svec -= newE
         Svec = (Svec 
                + np.matmul(Svec.reshape(1,n), realOD)
                - Svec * realOD.sum(axis=1)
+               - newV
                 )
         Evec = Evec + newE - newI
         Evec = (Evec 
@@ -108,9 +121,12 @@ def seir(par, distr, flow, alpha, iterations, inf):
         Rvec = (Rvec 
                + np.matmul(Rvec.reshape(1,n), realOD)
                - Rvec * realOD.sum(axis=1)
+               + newV
                 )
-                
-        res[iter + 1,:] = [Svec.sum(), Evec.sum(), Ivec.sum(), Rvec.sum(), 0]
+
+        Vvec += newV   
+        
+        res[iter + 1,:] = [Svec.sum(), Evec.sum(), Ivec.sum(), Rvec.sum(), 0, Vvec.sum()]
         eachIter[iter + 1] = newI.sum()
         res[iter + 1, 4] = eachIter[max(0, iter - par.HospitalIters) : iter].sum() * par.HospitalisationRate
         
@@ -118,32 +134,77 @@ def seir(par, distr, flow, alpha, iterations, inf):
         history[iter + 1,1,:] = Evec
         history[iter + 1,2,:] = Ivec
         history[iter + 1,3,:] = Rvec
-        
-        
+        history[iter + 1,5,:] = Vvec
+
+
     return res, history
 
-    def seir_plot_one_cell(res, cellid):
-        """ Plots SIR for a single cell
-        Parameters:
-        res: [3D matrix, compartment_id]
-        """
-        plt.plot(res[::12, 0, cellid], color='r', label='S') # Take every 12 value to get steps per day (beacause of 2-hours intervals) 
-        plt.plot(res[::12, 1, cellid], color='g', label='E')
-        plt.plot(res[::12, 2, cellid], color='b', label='I')
-        plt.plot(res[::12, 3, cellid], color='y', label='R')
-        plt.plot(res[::12, 4, cellid], color='c', label='H')
-        plt.legend()
-        plt.show()
+def seir_plot_one_cell(res, cellid):
+    """ Plots SIR for a single cell
+    Parameters:
+    res: [3D array, compartment_id]
+    """
+    plt.plot(res[::12, 0, cellid], color='r', label='S') # Take every 12 value to get steps per day (beacause of 2-hours intervals) 
+    plt.plot(res[::12, 1, cellid], color='g', label='E')
+    plt.plot(res[::12, 2, cellid], color='b', label='I')
+    plt.plot(res[::12, 3, cellid], color='y', label='R')
+    plt.plot(res[::12, 4, cellid], color='c', label='H')
+    plt.plot(res[::12, 5, cellid], color='m', label='V')
+    plt.legend()
+    plt.show()
 
-    def seir_plot(res):
-        """ Plots the epidemiological curves
-        Parameters:
-        res: [3D matrix, compartment_id]
-        """
-        plt.plot(res[::12, 0], color='r', label='S') # Take every 12 value to get steps per day (beacause of 2-hours intervals) 
-        plt.plot(res[::12, 1], color='g', label='E')
-        plt.plot(res[::12, 2], color='b', label='I')
-        plt.plot(res[::12, 3], color='y', label='R')
-        plt.plot(res[::12, 4], color='c', label='H')
-        plt.legend()
-        plt.show()
+def seir_plot(res):
+    """ Plots the epidemiological curves
+    Parameters:
+    res: [3D array, compartment_id]
+    """
+    plt.plot(res[::12, 0], color='r', label='S') # Take every 12 value to get steps per day (beacause of 2-hours intervals) 
+    plt.plot(res[::12, 1], color='g', label='E')
+    plt.plot(res[::12, 2], color='b', label='I')
+    plt.plot(res[::12, 3], color='y', label='R')
+    plt.plot(res[::12, 4], color='c', label='H')
+    plt.plot(res[::12, 5], color='m', label='V')
+    plt.legend()
+    plt.show()
+
+"""
+def main():
+
+    # load OD matrices
+    pkl_file = open('covid/data/data_counties/od_counties.pkl', 'rb') 
+    OD_matrices = pickle.load(pkl_file)
+    pkl_file.close()
+
+    # load vaccine schedule
+    pkl_file = open('covid/data/data_counties/vaccines_counties.pkl', 'rb') 
+    vacc = pickle.load(pkl_file)
+    pkl_file.close()
+    print(vacc.shape)
+
+    # create population 
+    df_befolkningstall_fylker = pd.read_csv("covid/data/data_counties/Folkemengde_fylker.csv", delimiter=";", skiprows=1)
+    befolkningsarray= df_befolkningstall_fylker['Befolkning per 1.1. (personer) 2020'].to_numpy(dtype='float64')
+    pop = np.asarray([befolkningsarray for _ in range(84)])
+
+    # Define simulation parameters
+    r = OD_matrices.shape[0]  # Simulation period (e.g 84)
+    n = pop.shape[1]          # Number of counties (e.g 11)
+    N = sum(befolkningsarray) # Total population (e.g 5367580)
+    initialInd = [2]          # Initial index of counties infected
+    initial = np.zeros(n)
+    initial[initialInd] = 50  # Number of infected people in each of the initial counties infected
+
+    model = Param(R0=2.4, DE=5.6*12, DI=5.2*12, I0=initial, HospitalisationRate=0.1, HospitalIters=15*12, eff=0.9) # multiply by 12 as one day consists of 12 2-hours periods 
+
+    alpha = np.ones(OD_matrices.shape)  # One == no quarantene influence. Multiplied by real flow.
+    iterations = 3000                    # Number of simulations
+    res = {}                            # Dictionary with results for different cases 
+    inf = 50                            # Number of random infections
+    res['baseline'] = seir(model, pop, OD_matrices, alpha, iterations, inf, vacc)
+    seir_plot(res["baseline"][0])
+    
+
+
+if __name__ == '__main__':
+    main()
+"""
