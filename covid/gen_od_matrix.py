81367968649df313b836c94f5faf50074a3b6a24
sandernordeide <nordeidesander@gmail.com>
2021-03-15 13:51:20 +0100

    Added municipalities in plots

--- covid/gen_od_matrix.py
+++ covid/gen_od_matrix.py
@@ -1,14 +1,14 @@
 import pickle
 import numpy as np
 
-num_counties = 11
+num_areas = 356
 num_time_steps = 84
 
 a = []
 for m in range(num_time_steps):
-    l = [[0 for x in range(num_counties)] for x in range(num_counties)] 
-    for i in range(0, num_counties):
-        for j in range(0, num_counties):
+    l = [[0 for x in range(num_areas)] for x in range(num_areas)] 
+    for i in range(0, num_areas):
+        for j in range(0, num_areas):
             if i == j:
                 l[i][j] = 0.8
             elif j == i+1:
@@ -19,10 +19,10 @@ for m in range(num_time_steps):
 
 arr = np.array(a)
 
-with open('peder\data_counties\od_counties.pkl','wb') as f:
+with open('data/data_municipalities/od_municipalities.pkl','wb') as f:
     pickle.dump(arr, f)
     
-with open('peder\data_counties\od_counties.pkl','rb') as f:
+with open('data/data_municipalities/od_municipalities.pkl','rb') as f:
      x = pickle.load(f)
      print(x[0])
      print(x.shape)
