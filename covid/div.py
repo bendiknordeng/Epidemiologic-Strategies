029f19d8bdd5e4dfe04d1fff32636b2dad8ab298
PederML <43098910+PederML@users.noreply.github.com>
2021-03-15 16:53:05 +0100

    All plotting and modelling work for municipalities
    All plotting and modelling work for municipalities

--- covid/div.py
+++ covid/div.py
@@ -1,19 +1,19 @@
-import pickle
+import pickle as pkl
 import numpy as np
 
-def generate_od_matrix(num_time_steps, num_counties):
+def generate_od_matrix(num_time_steps, num_regions):
     """ generate an OD-matrix used for illustrative purposes only
     Paramters:
-        num_counties:
+        num_regions:
         num_time_steps:
     Returns:
-        An OD-matrix with dimensions (num_time_steps, num_counties, num_counties) with 0.8 on its diagonal and 0.1 on cells next to diagonal 
+        An OD-matrix with dimensions (num_time_steps, num_regions, num_regions) with 0.8 on its diagonal and 0.1 on cells next to diagonal 
     """
     a = []
     for m in range(num_time_steps):
-        l = [[0 for x in range(num_counties)] for x in range(num_counties)] 
-        for i in range(0, num_counties):
-            for j in range(0, num_counties):
+        l = [[0 for x in range(num_regions)] for x in range(num_regions)] 
+        for i in range(0, num_regions):
+            for j in range(0, num_regions):
                 if i == j:
                     l[i][j] = 0.8
                 elif j == i+1:
@@ -23,7 +23,7 @@ def generate_od_matrix(num_time_steps, num_counties):
         a.append(l)
     return np.array(a)
 
-def generate_vaccine_matrix(num_counties=11, num_time_steps=84):
+def generate_vaccine_matrix(num_time_steps, num_regions):
     """ generate an vaccine matrix used for illustrative purposes only
     Paramters:
         num_counties:
@@ -31,7 +31,7 @@ def generate_vaccine_matrix(num_counties=11, num_time_steps=84):
     Returns:
         An vaccine matrix with dimensions (num_time_steps, num_counties) 
     """
-    a = [[100 for x in range(num_counties)] for i in range (num_time_steps)]
+    a = [[10 for x in range(num_regions)] for i in range (num_time_steps)]
     return np.array(a)
     
 def write_pickle(filepath, arr):
@@ -41,7 +41,7 @@ def write_pickle(filepath, arr):
         arr: array that is written to file 
     """
     with open(filepath,'wb') as f:
-        pickle.dump(arr, f)
+        pkl.dump(arr, f)
 
 def read_pickle(filepath):
     """ read pickle from filepath
@@ -51,7 +51,7 @@ def read_pickle(filepath):
         arr: array that is read from filepath 
     """
     with open(filepath,'rb') as f:
-        return pickle.load(f)
+        return pkl.load(f)
 
 
 def main():
@@ -62,27 +62,19 @@ def main():
     # print(x[0])
     # print(x.shape)
 
-    f = 'covid/data/data_municipalities/od_municipalities.pkl'
-    od = generate_od_matrix(84, 356)
-    write_pickle(f, od)
-    x = read_pickle(f)
-    print(x[0])
-    print(x.shape)
+    # f = 'covid/data/data_municipalities/od_municipalities.pkl'
+    # od = generate_od_matrix(84, 356)
+    # write_pickle(f, od)
+    # x = read_pickle(f)
+    # print(x[0])
+    # print(x.shape)
 
-<<<<<<< HEAD
-    v = 'data/data_municipalities/vaccines_municipalities.pkl'
-    m = generate_vaccine_matrix(num_counties=356)
+    v = 'covid/data/data_municipalities/vaccines_municipalities.pkl'
+    m = generate_vaccine_matrix(84, 356)
     write_pickle(v, m)
     y = read_pickle(v)
-=======
-
-    # v = 'covid/data/data_counties/vaccines_counties.pkl'
-    # m = generate_vaccine_matrix()
-    # write_pickle(v, m)
-    # y = read_pickle(v)
->>>>>>> 272ec5e85870afc762cbbb4f9b99e744ac8a67cd
-    # print(y)
-    # print(y.shape)
+    print(y)
+    print(y.shape)
 
 if __name__ == '__main__':
     main()
