26e8425d031037e82598afdeae5f362beb3eaa5a
sandernordeide <nordeidesander@gmail.com>
2021-03-22 13:21:50 +0100

    organize filepaths as namedtuple

--- covid/main.py
+++ covid/main.py
@@ -3,23 +3,16 @@ from collections import namedtuple
 from utils import read_config
 
 if __name__ == '__main__':
-    # filepaths 
-    fpath_config                     = 'covid/configs/baseline.txt'
-    fpath_od                         = 'covid/data/data_municipalities/od_municipalities.pkl'
-    fpath_muncipalities_names        = 'covid/data/data_municipalities/Kommunenummer_navn_2020.csv'
-    fpath_muncipalities_pop          = 'covid/data/data_municipalities/Folkemengde_kommuner.csv'
-    fpath_municipalities_v           = 'covid/data/data_municipalities/vaccines_municipalities.pkl'
-    fpath_municipalities_geo_pkl     = 'covid/data/data_municipalities/norge_geojson.pkl'
-    fpath_municipalities_geo_geojson = 'covid/data/data_municipalities/Basisdata_0000_Norge_25833_Kommuner_GEOJSON.geojson'
-    fpath_municipality_gif           = 'covid/gifs/gifs_municipalities/Covid_19_municipalities.gif'
+    # read filepaths 
+    paths = read_config('filepaths.txt')
 
     # read in data from filepaths 
-    config = read_config(fpath_config)
-    OD_matrices = load_od_matrices(fpath_od)
-    pop, befolkning = create_population(fpath_muncipalities_names, fpath_muncipalities_pop)
+    config = read_config(paths.config)
+    OD_matrices = load_od_matrices(paths.od)
+    pop, befolkning = create_population(paths.muncipalities_names, paths.muncipalities_pop)
     seir = initialize_seir(config, pop.shape[1], 50)
-    vacc = load_vaccination_programme(OD_matrices.shape[0], pop.shape[1], fpath_municipalities_v)
-    kommuner_geometry = create_geopandas(True, befolkning, fpath_municipalities_geo_pkl, fpath_municipalities_geo_geojson)
+    vacc = load_vaccination_programme(OD_matrices.shape[0], pop.shape[1], paths.municipalities_v)
+    kommuner_geometry = create_geopandas(True, befolkning, paths.municipalities_geo_pkl, paths.municipalities_geo_geojson)
     
     # simulate seir 
     res = {}   # Dictionary with the results for all cases {}                         
@@ -35,7 +28,7 @@ if __name__ == '__main__':
     hosp = res['baseline'][0][::12, 4]
 
     # geospatial plots 
-    plot_simulation(baseline, befolkning, hosp, kommuner_geometry)
+    plot_simulation(baseline, befolkning, hosp, kommuner_geometry, paths.municipality_plots)
 
     # generate gif 
-    create_gif(fpath_municipality_gif)
+    create_gif(paths.municipality_gif,paths.municipality_plots)
