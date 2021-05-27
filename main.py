2f1c6d8e8bbb15830d141fd286d14055acb807a4
PederML <43098910+PederML@users.noreply.github.com>
2021-05-27 15:14:25 +0200

    Small change in SEAIR.py
    Small change in SEAIR.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import os
 
 if __name__ == '__main__':
     # Set initial parameters
-    runs = 200
+    runs = 1
     decision_period = 28
     start_day, start_month, start_year = 24, 2, 2020
     start_date = utils.get_date(f"{start_year}{start_month:02}{start_day:02}")
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     policies = ['random', 'no_vaccines', 'susceptible_based', 
                 'infection_based', 'oldest_first', 'contact_based', 
                 'weighted', 'fhi_policy']
-    policy_number = 0
+    policy_number = 2
     weights = np.array([0, 0, 0, 1, 0])
 
     # Read data and generate parameters
