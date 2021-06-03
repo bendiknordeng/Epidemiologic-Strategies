5410b1f07b3307c33cdfc1c0cb4a322448aa4be1
Bendik Nordeng <bendiknor@gmail.com>
2021-06-03 21:03:46 +0200

    Fix error occuring in ge_wave_timeline

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import os
 
 if __name__ == '__main__':
     # Set initial parameters
-    runs = 1
+    runs = 10
     decision_period = 56
     start_day, start_month, start_year = 24, 2, 2020
     start_date = utils.get_date(f"{start_year}{start_month:02}{start_day:02}")
@@ -41,7 +41,7 @@ if __name__ == '__main__':
     historic_data = utils.get_historic_data()
     
     # Run settings
-    run_GA = True
+    run_GA = False
     include_flow = True
     stochastic = True
     use_wave_factor = True
